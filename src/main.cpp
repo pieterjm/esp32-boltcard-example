2bd5373eace57f49c28fabc02afa09092ed51e38
Pieter Meulenhoff <pieter.meulenhoff@eurofins-cybersecurity.com>
2023-11-06 13:41:44 +0100

    removed Wi-Fi and added Lolin S2 Mini

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@
 #ifdef USE_I2C
 #include <Wire.h>
 #endif
-#include <HTTPClient.h>
-#include <ArduinoJson.h>
 
 
 // Use this line for a breakout with a software SPI connection (recommended):
@@ -19,20 +17,33 @@ Adafruit_PN532 nfc(PN532_SCL,PN532_SDA);
 
 
 void setup(void) {
-  WiFi.begin(WIFI_SSID,WIFI_PWD);
-  
-
   Serial.begin(115200);
-  while (!Serial)
-    delay(10); // for Leonardo/Micro/Zero
+  delay(1000);
 
+  Serial.println("Hello");
+  delay(1000);
+
+  Serial.println("Hello");
+  delay(1000);
+
+  Serial.println("Hello");
+  delay(1000);
+
+  Serial.println("Hello");
+
+  //WiFi.begin(WIFI_SSID,WIFI_PWD);
+
+  Serial.println("Hello");
+  
   nfc.begin();
 
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (!versiondata) {
     Serial.print("Didn't find PN53x board");
-    while (1)
-      ; // halt
+    while (1) {
+      Serial.println("Could not find PN532");
+      sleep(1000);
+    }
   }
   // Got ok data, print it out!
   Serial.print("Found chip PN53x");
@@ -49,113 +60,6 @@ void setup(void) {
 }
 
 
-bool make_lnurlw_withdraw(String lnurlw, int amount, String memo = "") {
-  if ( ! lnurlw.startsWith("lnurlw://")) {
-    Serial.println("This is not an LNURLW");
-    return false;
-  }
-  
-  // create URL
-  String url = String("https://") + lnurlw.substring(9);
-  
-  HTTPClient http;
-  http.begin(url);
-  int statusCode = http.GET();
-  if ( statusCode != HTTP_CODE_OK ) {
-    Serial.println("HTTP request not accepted for LNURLW");
-    return false;
-  }
-
-  String payload = http.getString();
-  http.end();
-  
-  DynamicJsonDocument doc(2000);
-  DeserializationError error = deserializeJson(doc, payload.c_str());
-  if ( error != DeserializationError::Ok ) {
-    Serial.println("Failed to parse JSON ");
-    return false;
-  }
-
-  // extract callback URL and k1
-  String callback = doc["callback"].as<String>();
-  String k1 = doc["k1"].as<String>();
-  String status = doc["status"].as<String>();
-  String reason = doc["reason"].as<String>();
-  if (( status != NULL ) && status.equals("ERROR")) {
-    Serial.println("Error in LNURLW response");
-    Serial.println(reason);
-    return false;
-  }
-
-  if ( callback == NULL ) {
-    Serial.println("No callback in LNURLW response");
-    return false;
-  }
-
-  // create an invoice, for now we ignore the min/max of the card
-  http.begin(INVOICE_URL);
-  http.addHeader("X-Api-Key",INVOICE_KEY);
-  http.addHeader("Content-type","application/json");
-
-  // create invoice data
-  doc.clear();
-  doc["out"] = false;
-  doc["amount"] = amount;
-  doc["memo"] = memo.c_str();
-  payload.clear();
-  serializeJson(doc, payload);
-
-  statusCode = http.POST(payload);
-  if ( statusCode != HTTP_CODE_CREATED) {
-    Serial.println("Failed to create invoice");
-    Serial.println(statusCode);
-    Serial.printf("POST payload='%s'\n",payload.c_str());
-    return false;
-  } 
-  payload = http.getString();
-
-
-  error = deserializeJson(doc, payload.c_str());
-  if ( error != DeserializationError::Ok ) {
-    Serial.println("Failed to parse invoice ");
-    return false;
-  }
-
-
-  String payment_request = doc["payment_request"].as<String>();
-
-
-  url = callback + "?k1=" + k1 + "&pr=" + payment_request;
-  Serial.println(url);            
-
-  http.begin(url);
-  statusCode = http.GET();
-  if ( statusCode != HTTP_CODE_OK ) {
-    Serial.println("HTTP request not accepted for callback");
-    return false;
-  }
-
-  payload = http.getString();
-  http.end();
-  Serial.println(payload);
-
-  error = deserializeJson(doc, payload.c_str());
-  if ( error != DeserializationError::Ok ) {
-    Serial.println("Failed to parse payment result ");
-    return false;
-  }
- 
-  status = doc["status"].as<String>();  
-
-  if ( status.equals("OK") ) {
-    Serial.println("Payment succesfull");
-    return true;
-  } else {
-    Serial.println("Payment failed");
-    return false;
-  }
-
-}
 
 void loop(void) {
   uint8_t success;
@@ -183,7 +87,8 @@ void loop(void) {
       uint8_t buffer[512];
       uint8_t bytesread = nfc.ntag424_ISOReadFile(buffer);
       String lnurlw = String((char *)buffer,bytesread);
-      make_lnurlw_withdraw(lnurlw, 21,"Some memo");
+      Serial.println("This is what I read from the card: ");
+      Serial.println(lnurlw);
     } else {
       Serial.println("This doesn't seem to be an NTAG424 tag. (UUID length != "
                      "7 bytes and UUID length != 4)!");
